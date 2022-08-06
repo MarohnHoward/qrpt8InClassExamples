1f67cadfbe9979da4ffcf2a01dcaf7c9237db476
Haley Firth <hvinsonfirth@gmail.com>
2022-08-06 09:51:53 -0600

    Made a smoll lil comment

---
+++ Unit2.1/settingVariables.ts
@@ -0,0 +1,14 @@
+/** firstName is a string  */
+var firstName: string = "Bob"
+
+const profileName: string = "BobsNotABuilder"
+let userBio: string = "Stop asking if I can fix it. Cause no I can't"
+var phoneNumber: number = 8018675309
+
+console.log("Should be Bob", firstName) // this will log the firstName Var
+console.log("Should be Bobs profileName", profileName)
+// this will input profileName 
+console.log("Should be userbio", userBio)
+/*this is for userBio */
+console.log("should be a phone number", phoneNumber)
+
